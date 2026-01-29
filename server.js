d495f06176cb6a24c67274091d64e8f852bab352
Mbas <mbas750@gmail.com>
2026-01-29 14:31:31 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
